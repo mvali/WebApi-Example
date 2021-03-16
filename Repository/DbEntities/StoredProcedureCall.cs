81ac0937fcf39c9558b542bf6055ea1a12b58400
mvali <vali_my@yahoo.com>
2021-03-16 23:22:25 +0200

    db

--- Repository/DbEntities/StoredProcedureCall.cs
+++ Repository/DbEntities/StoredProcedureCall.cs
@@ -53,15 +53,10 @@ namespace Repository.DbEntities
     public class GuestArrival
     {
         public string Forename { get; set; }
-
         public string Surname { get; set; }
-
         public string TelNo { get; set; }
-
         public DateTime From { get; set; }
-
         public DateTime To { get; set; }
-
         public int RoomNumber { get; set; }
     }
 
